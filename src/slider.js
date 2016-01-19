26a41461107a61d14b375ee5c07963c49623524a
Adrien Rambert <adrien@rambert.me>
2016-01-19 10:36:44 +0100

    Add tip option to display the value next to the handle (or in the handle)

--- src/slider.js
+++ src/slider.js
@@ -110,6 +110,7 @@ angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider',
 
 
                     ngModel.$setViewValue(ui.values || ui.value);
+                    $(ui.handle).find('.ui-slider-tip').text(ui.value);
                     scope.$apply();
 
                     if (valuesChanged) {
@@ -125,7 +126,7 @@ angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider',
                 ngModel.$render = function() {
                     init();
                     var method = options.range === true ? 'values' : 'value';
-                    
+
                     if (options.range !== true && isNaN(ngModel.$viewValue) && !(ngModel.$viewValue instanceof Array)) {
                         ngModel.$viewValue = 0;
                     }
@@ -136,7 +137,7 @@ angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider',
                     // Do some sanity check of range values
                     if (options.range === true) {
                         // previously, the model was a string b/c it was in a text input, need to convert to a array.
-                        // make sure input exists, comma exists once, and it is a string. 
+                        // make sure input exists, comma exists once, and it is a string.
                         if (ngModel.$viewValue && angular.isString(ngModel.$viewValue) && (ngModel.$viewValue.match(/,/g) || []).length === 1) {
                             // transform string model into array.
                             var valueArr = ngModel.$viewValue.split(',');
@@ -190,7 +191,7 @@ angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider',
                 // Add tick marks if 'tick' and 'step' attributes have been setted on element.
                 // Support horizontal slider bar so far. 'tick' and 'step' attributes are required.
                 var options = angular.extend({}, scope.$eval(attrs.uiSlider));
-                var properties = ['max', 'step', 'tick'];
+                var properties = ['max', 'step', 'tick', 'tip'];
                 angular.forEach(properties, function(property) {
                     if (angular.isDefined(attrs[property])) {
                         options[property] = attrs[property];
@@ -202,6 +203,9 @@ angular.module('ui.slider', []).value('uiSliderConfig',{}).directive('uiSlider',
                         var left = ((i / total) * 100) + '%';
                         $("<div/>").addClass("ui-slider-tick").appendTo(element).css({left: left});
                     };
+                }
+                if(angular.isDefined(options['tip'])) {
+                    $timeout(function(){element.find('.ui-slider-handle').append('<div class="ui-slider-tip">'+ngModel.$viewValue+'</div>')},10);
                 }
             }
 
